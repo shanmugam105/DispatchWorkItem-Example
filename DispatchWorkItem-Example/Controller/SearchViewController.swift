1ff50c7dc1dc8b973030526c70bb1e471664742c
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2022-04-11 20:40:24 +0530

    Initial Commit

---
+++ DispatchWorkItem-Example/Controller/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  DispatchWorkItem-Example
+//
+//  Created by Mac on 11/04/22.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ DispatchWorkItem-Example/Delegate/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  DispatchWorkItem-Example
+//
+//  Created by Mac on 11/04/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
+
