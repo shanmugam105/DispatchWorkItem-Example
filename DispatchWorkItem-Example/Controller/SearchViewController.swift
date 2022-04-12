0232f12c57261880ace07683da194a0a9eadc2ff
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2022-04-12 07:46:05 +0530

    Basic DispatchWorkItem setup done

--- DispatchWorkItem-Example/Controller/SearchViewController.swift
+++ DispatchWorkItem-Example/Controller/SearchViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class SearchViewController: UITableViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchWork: DispatchWorkItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        searchWork?.cancel()
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        findUser(for: searchText)
+    }
+    
+    private func findUser(for string: String) {
+        searchWork?.cancel()
+        let work = DispatchWorkItem {
+            print("Hello, \(string)!")
+        }
         
+        work.notify(queue: .main) {
+            print("Search finished!")
+        }
+        searchWork = work
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2, execute: work)
     }
 }
 
